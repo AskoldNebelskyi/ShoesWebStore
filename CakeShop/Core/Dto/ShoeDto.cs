60b690a7fc903b003709ae07d9162f55ce05d3e5
AskoldNebelskyi <askoldnebelskyi@gmail.com>
2022-06-22 03:36:05 +0300

    AddShoe form improved

--- CakeShop/Core/Dto/ShoeDto.cs
+++ CakeShop/Core/Dto/ShoeDto.cs
@@ -6,32 +6,33 @@ namespace ShoeShop.Core.Dto
     {
         public int Id { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Одмен, що це за шузи?")]
         [Display(Name = "Назва шузів")]
 
         public string Name { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Ну хоч два слова!")]
         [Display(Name = "Короткий опис")]
         [MaxLength(50)]
         public string ShortDescription { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Покупцям важливо знати інфу!")]
         [Display(Name = "Опис")]
         [MaxLength(255)]
         public string LongDescription { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "ХАВ МАЧ ЦІ ШУЗА КОШТУЮТЬ?")]
         [Display(Name = "Прайс")]
         public decimal Price { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Їх мусять побачити!")]
         [Display(Name = "Url зображення")]
         public string ImageUrl { get; set; }
 
         [Display(Name = "Вибір тижня? ")]
         public bool IsShoeOfTheWeek { get; set; }
 
+        [Required(ErrorMessage = "А категорія?)")]
         [Display(Name = "Категорія")]
         public int CategoryId { get; set; }
     }
